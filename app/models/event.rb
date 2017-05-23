6472993e6729b31704be3ea8bfaa879bdfaf71ae
Claudia413 <cmjengelsman@gmail.com>
2017-05-23 14:30:22 +0200

    Add view for events to user page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
-  validates :starts_at, presence: true
-  validates :ends_at, presence: true
+  validates :starts_at, presence: false
+  validates :ends_at, presence: false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+claudia = User.create!(email: "claudia@me.com", password: "test12")
+
+event = Event.create!(name: "Gittyhub", description:"Let's add some random text here to describe this awesome event Gittyhub! Gittyhub's first meeting for people who are learning to use Github and sometime have no idea what they are doing and just hope it all works out if they press a button.",  location:"Singel 542, 1B, 1234AB Amsterdam, the Netherlands", price: 0, capacity: 25, includes_food: false, includes_drinks: true, active: true, user: claudia)
