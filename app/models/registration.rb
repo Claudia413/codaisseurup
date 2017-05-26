a7b5ea31fd61dd2cbe1eb76d3a8551fa94262911
Claudia413 <cmjengelsman@gmail.com>
2017-05-26 14:06:16 +0200

    Add Active Record callback

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,6 +2,12 @@ class Registration < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
+  before_save :set_price
+
+  def set_price
+      price = self.event.price
+  end
+
   def set_total_price
     self.price = event.price
     self.total = price * guests_count
