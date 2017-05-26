f8eb64e86e952b469ee101ba65a8eb4f7aa72118
Claudia413 <cmjengelsman@gmail.com>
2017-05-26 13:22:59 +0200

    Add event registration and set max nr of guests to sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,11 @@ class RegistrationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-      @registration = current_user.registrations.create(registration_params)
+      @registration = current_user.registrations.create(registration_params.merge(event_id: params[:event_id]))
+    @registration.set_total_price
+    @registration.save
 
-      redirect_to @registration.event, notice: "Thank you for registration!"
+      redirect_to @registration.event, notice: "Thank you for registering!"
     end
 
     private
